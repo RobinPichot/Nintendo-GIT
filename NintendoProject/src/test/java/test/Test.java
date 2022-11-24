9789cd1e919b114cd062431e8855dafed9a36727
Thomas <ourliac.du.82@gmail.com>
2022-11-24 12:58:27 +0100

    rectification test

--- NintendoProject/src/test/java/test/Test.java
+++ NintendoProject/src/test/java/test/Test.java
@@ -11,7 +11,6 @@ public class Test {
 		// TODO Auto-generated method stub
 
 	}
-<<<<<<< HEAD
 	Boutique b1 = new Boutique("Micromania","Toulouse");
 	Console ps5 = new Salon ("PS5");
 	Jeu j1 = new Jeu ("FIFA23",ps5,b1);
@@ -19,14 +18,6 @@ public class Test {
 	Jeu j3 = new Jeu ("Rocket League",ps5,b1);
 	Jeu j4 = new Jeu ("GTA VI",ps5,b1);
 	Jeu j5 = new Jeu ("Fall Guys",ps5,b1);
-=======
-	Console ps5 = new Console ("PS5");
-	Jeu j1 = new Jeu ("FIFA23",ps5);
-	Jeu j2 = new Jeu ("God of War",ps5);
-	Jeu j3 = new Jeu ("Rocket League",ps5);
-	Jeu j4 = new Jeu ("GTA VI",ps5);
-	Jeu j5 = new Jeu ("Fall Guys",ps5);
-	
-	Boutique b1 = new Boutique("Claire's","1 place Capitole"); 
->>>>>>> main
+	Boutique b2 = new Boutique("Claire's","1 place Capitole"); 
+
 }
