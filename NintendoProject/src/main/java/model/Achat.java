a5c606fe8aec7fc17215845100a5b9e6f9ee952d
Robin <robin.pichot@gmail.com>
2022-11-24 12:44:12 +0100

    Ajout classe achat

---
+++ NintendoProject/src/main/java/model/Achat.java
@@ -0,0 +1,43 @@
+package model;
+
+public class Achat {
+
+	
+	private Jeu jeu;
+	private String date;
+	private double prix;
+	
+	
+	
+	
+	
+	public Achat(Jeu jeu, String date, double prix) {
+		this.jeu = jeu;
+		this.date = date;
+		this.prix = prix;
+	}
+	public Jeu getJeu() {
+		return jeu;
+	}
+	public void setJeu(Jeu jeu) {
+		this.jeu = jeu;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public double getPrix() {
+		return prix;
+	}
+	public void setPrix(double prix) {
+		this.prix = prix;
+	}
+	@Override
+	public String toString() {
+		return "Achat [jeu=" + jeu + ", date=" + date + ", prix=" + prix + "]";
+	}
+	
+	
+}
